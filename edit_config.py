4f6893de57e8a78eb338b50c42e520174b028a10
junjenchan <junjenchan@gmail.com>
2015-05-03 10:37:09 -0700

    edited the edit_config.py file

--- edit_config.py
+++ edit_config.py
@@ -5,10 +5,10 @@ output = open('temp.ini','w')
 for line in input.readlines():
     if line == "[couchdb]\n":
         output.write(line + "database_dir = /mnt/data/couchdb\n")
-		output.write("view_index_dir = /mnt/data/couchdb\n")
+        output.write("view_index_dir = /mnt/data/couchdb\n")
     elif line == "[httpd]\n":
-		output.write(line + "bind_address = 0.0.0.0\n")
-	else:
+        output.write(line + "bind_address = 0.0.0.0\n")
+    else:
         output.write(line)
 
 call(['mv', 'temp.ini', '/etc/couchdb/local.ini'])
